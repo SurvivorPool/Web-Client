0ed823406e831e665f35e7ba150c9736accb50db
Jimmy Timmons <timmonsjg@gmail.com>
2018-05-16 22:26:54 -0400

    Sass setup / initial structuring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import store from './configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
+import router from "./Router/router";
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <div className={"App"}>
+            {router}
+        </div>
     </Provider>, document.getElementById('root'));
 registerServiceWorker();
