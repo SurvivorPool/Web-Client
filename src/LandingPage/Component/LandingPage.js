d7caf24cf43098f51157d807aeff6335e9f93aa1
Jimmy Timmons <timmonsjg@gmail.com>
2018-05-19 17:13:53 -0400

    color changes

--- src/LandingPage/Component/LandingPage.js
+++ src/LandingPage/Component/LandingPage.js
@@ -21,7 +21,9 @@ class LandingPage extends Component {
 				</header>
 				<section className={sectionClassName}>
 					<div className={`${sectionClassName}__Content`}>
-						<p>{"How it Works Text"}</p>
+						<p>{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eros mauris, volutpat vitae auctor ut, semper et arcu. Sed tempor ultricies viverra. Sed ornare ante eget porta sollicitudin. Etiam facilisis lacus et elementum commodo. Phasellus non nisl euismod, aliquam risus quis, egestas felis. Suspendisse semper ante tellus, id dignissim neque efficitur at. Mauris eu ex non libero laoreet molestie congue sodales arcu. Duis hendrerit et sapien vitae egestas. Suspendisse non tristique justo, in scelerisque erat. Integer vel eros at purus interdum laoreet sed ullamcorper magna."}</p>
+						<p>{"Cras finibus mollis odio, nec volutpat sem blandit non. Nulla malesuada diam nec urna mollis, dignissim congue quam imperdiet. In sed lectus nulla. Etiam varius nisi eu nibh accumsan dictum. Donec a ultricies felis. Vivamus vitae ultricies nisi, at porta risus. Nullam a neque porta, consectetur ex a, feugiat leo. Nulla maximus justo nec sem pretium viverra. Sed sed quam erat."}</p>
+						<p>{"Morbi consequat malesuada mauris, tincidunt placerat libero congue vel. Nullam varius, nisl at tempus maximus, nunc leo dapibus libero, nec sodales dolor felis ut ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce eu molestie sapien. Morbi sollicitudin nunc nec enim sodales, nec bibendum magna consectetur. Vivamus nunc dolor, consequat at odio eu, pellentesque finibus sapien. Donec eget elementum risus. Sed enim ex, hendrerit nec luctus id, porta nec massa. Nunc auctor lacinia volutpat. Nam tincidunt metus suscipit velit mollis, quis imperdiet magna interdum. Cras eu est non nulla hendrerit aliquam. In odio tortor, vulputate eu mollis eget, pellentesque eu magna."}</p>
 					</div>
 				</section>
 			</div>
