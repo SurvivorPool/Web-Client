46e3cf61d6fb2fd63813f6b387476f01100ad60b
Jimmy Timmons <timmonsjg@gmail.com>
2018-05-28 16:46:37 -0400

    refactor out login modal

--- src/LandingPage/Component/LandingPage.js
+++ src/LandingPage/Component/LandingPage.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import { Image, Icon, Button, Segment, Divider } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 import autoBind from 'react-autobind';
-import ReactModal from 'react-modal';
 
 import Navbar from "../../Navbar/Component/Navbar";
-import PrimaryButton from "../../Common/PrimaryButton/PrimaryButton";
+import LoginModal from './LoginModal';
 
 import logo from 'Images/logo.png';
-import { HelmetIcon, RefereeIcon } from "Icons/Icons.js";
+import { HelmetIcon } from "Icons/Icons.js";
 
 const className = "LandingPage";
 const heroClassName = `${className}__Hero`;
 const sectionClassName = `${className}__Section`;
-const modalClassName = `${className}__Modal`;
 
 class LandingPage extends Component {
 	constructor(props) {
@@ -37,44 +35,10 @@ class LandingPage extends Component {
 	}
 
 	renderLoginModal() {
-		return (
-			<ReactModal
-				isOpen={this.state.isLoggingIn}
-				className={modalClassName}
-				overlayClassName={`${modalClassName}__Overlay`}
-			>
-				<Segment stacked>
-					<div className={`${modalClassName}__Content`}>
-						<div className={`${modalClassName}__Controls`}>
-							<PrimaryButton onClick={this.onCloseModalClick}>{"Close"}</PrimaryButton>
-						</div>
-						<div className={`${modalClassName}__Login`}>
-							<div className={`${modalClassName}__Login__Title`}>
-								<RefereeIcon
-									className={`${modalClassName}__Login__Icon`}
-								/>
-								<h1 className={`${modalClassName}__Login__h1`}>{"Login"}</h1>
-							</div>
-							<Divider />
-							<Button.Group vertical className={`${modalClassName}__Login__Buttons`}>
-								<Button color={'facebook'}>
-									<Icon name={'facebook'} />
-									{"Facebook"}
-								</Button>
-								<Button color={'google plus'}>
-									<Icon name={'google'} />
-									{"Google"}
-								</Button>
-								<Button color={'grey'}>
-									<Icon name={'github'} />
-									{"Github"}
-								</Button>
-							</Button.Group>
-						</div>
-					</div>
-				</Segment>
-			</ReactModal>
-		);
+		return <LoginModal
+			isOpen={this.state.isLoggingIn}
+			onClose={this.onCloseModalClick}
+		/>
 	}
 
 	render() {
