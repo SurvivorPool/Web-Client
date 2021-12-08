849d0cf6a1833cd936472123df620834cf81d551
Jimmy Timmons <hi@jimmy.dev>
2021-12-08 01:15:22 -0500

    update directory & dockerize

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+import { Container } from "./styled";
+
+const Nav: FC = ({ children }) => <Container>{children}</Container>;
+
+export default Nav;
---
+++ src/components/Nav/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+import { device } from "@/utils/device";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 75px;
+    display: flex;
+    justify-content: flex-end;
+`;
